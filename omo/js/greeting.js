39baa109ed6242c04b20ca3e0b63df2f09d8e98d
stay-plz <71306981+stay-plz@users.noreply.github.com>
2020-09-25 00:06:33 +0900

    1

--- omo/js/greeting.js
+++ omo/js/greeting.js
@@ -1,11 +1,18 @@
 const greetingContainer = document.querySelector(".js-greeting");
 const greetingInput = greetingContainer.querySelector("input");
-const greetingText = document.querySelector(".js-greetingtxt");
-const delBtn = document.querySelector(".js-delgreeting");
+const greetingTitle = document.querySelector(".js-greeting__title");
+const delBtn = document.querySelector(".js-greeting__del-title");
 
 const LS_KEY = "currentUser";
-const CN_SHOW = "show";
-const CN_HIDE = "hide";
+const CN_HIDE = "hiding";
+const CN_SHOW = "showing";
+
+function showing(container) {
+  if (container.classList.contains(CN_HIDE)) {
+    container.classList.remove(CN_HIDE);
+  }
+  container.classList.add(CN_SHOW);
+}
 
 function hiding(container) {
   if (container.classList.contains(CN_SHOW)) {
@@ -14,50 +21,38 @@ function hiding(container) {
   container.classList.add(CN_HIDE);
 }
 
-function shoiwng(container) {
-  if (container.classList.contains(CN_HIDE)) {
-    container.classList.remove(CN_HIDE);
-  }
-  container.classList.add(CN_SHOW);
-}
-
-function showName() {
-  shoiwng(greetingText);
-  greetingText.innerText = localStorage.getItem(LS_KEY);
-}
-
-function saveName(name) {
-  localStorage.setItem(LS_KEY, name);
+function showName(name) {
+  greetingTitle.innerText = name;
   hiding(greetingContainer);
-  showName();
+  showing(greetingTitle);
 }
-
-function askName() {
-  hiding(greetingText);
-  shoiwng(greetingContainer);
+function saveName() {
+  showing(greetingContainer);
+  hiding(greetingTitle);
   greetingContainer.addEventListener("submit", function (event) {
     event.preventDefault();
-    const name = greetingInput.value;
-    saveName(name);
+    let name = greetingInput.value;
+    localStorage.setItem(LS_KEY, name);
+    showName(name);
   });
 }
 
 function loadName() {
-  const NAME_LS = localStorage.getItem(LS_KEY);
-  if (NAME_LS === null || NAME_LS === "undifined") {
-    askName();
+  const LS_VALUE = localStorage.getItem(LS_KEY);
+  if (LS_VALUE === null) {
+    saveName();
   } else {
+    greetingTitle.innerText = LS_VALUE;
     hiding(greetingContainer);
-    showName();
+    showing(greetingTitle);
   }
 }
 
 function init() {
-  delBtn.addEventListener("click", function () {
-    greetingText.innerHTML = "";
+  loadName();
+  delBtn.addEventListener("click", function (event) {
     localStorage.removeItem(LS_KEY);
     loadName();
   });
-  loadName();
 }
 init();
